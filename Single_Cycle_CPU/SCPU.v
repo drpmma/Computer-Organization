d6186905cfe388941d6d81e5a6d908d0da97c7c0
ZhouxZhan <zhouxzhan@zju.edu.cn>
2017-04-29 15:32:59 +0800

    update frame

--- Single_Cycle_CPU/SCPU.v
+++ Single_Cycle_CPU/SCPU.v
@@ -43,17 +43,17 @@ module SCPU(clk,
 	output [31:0] PC_out;
 	
 	wire [31:0]o_pc;
-	wire [31:0]i_pc;
+	wire [31:0]i_pc = 0;
 	wire [31:0]pc_4;
 	
 	assign PC_out = o_pc;
 
 	wire [1:0]ALUop;
 	
-	single_pc PC(.clk(clk), .rst(reset), .i_pc(), .o_pc(o_pc));
-	single_pc_plus4 PC_4(.i_pc(o_pc), .o_pc(pc_4));
-	control CTRL(.inst(inst_in[31:26]), .RegDst(RegDst), .Branch(), .MemRead(), .MemtoReg(), 
-				 .ALUop(ALUop), .MemWrite(), .ALUSrc(ALUSrc), .RegWrite(RegWrite));
+	single_pc PC(.clk(clk), .rst(reset), .i_pc(i_pc), .o_pc(o_pc));
+	add_32 PC_4(.a(o_pc), .b(4), .c(pc_4));
+	control CTRL(.inst(inst_in[31:26]), .RegDst(RegDst), .Branch(), .MemRead(), .MemtoReg(MemtoReg), 
+				 .ALUop(ALUop), .MemWrite(MemWrite), .ALUSrc(ALUSrc), .RegWrite(RegWrite));
 				 
 	wire [4:0]w_reg;
 
@@ -62,23 +62,32 @@ module SCPU(clk,
 	assign Data_out = rdata_B;			 
 	
 	regs Registers(.clk(clk), .rst(reset), .reg_Rd_addr_A(inst_in[25:21]), .reg_Rt_addr_B(inst_in[20:16]), .reg_Wt_addr(w_reg), 
-					   .wdata(Data_in), .we(RegWrite), .rdata_A(rdata_A), .rdata_B(rdata_B));
+					   .wdata(w_data), .we(RegWrite), .rdata_A(rdata_A), .rdata_B(rdata_B));
 	wire [31:0]sign_shamt;
 	Ext_32 se(.imm_16(inst_in[15:0]), .Imm_32(sign_shamt));
+
+	wire [31:0]offset;
+	add_32 Add(.a(pc_4), .b(sign_shamt << 2), .c(offset));
+	MUX2T1_32 mux2_32_1(.I0(pc_4), .I1(offset), .s(), .o(i_pc))
+	
 	wire [31:0]ALU_B;
 	
-	MUX2T1_32 mux2_32(.I0(rdata_B), .I1(sign_shamt), .s(ALUSrc), .o(ALU_B));
+	MUX2T1_32 mux2_32_2(.I0(rdata_B), .I1(sign_shamt), .s(ALUSrc), .o(ALU_B));
 	
 	ALU_ctrl alu_ctrl();
 	
 	wire [31:0]ALU_res;
 
 	assign Addr_out = ALU_res;
+	assign mem_w = MemWrite;
 
 	ALU alu(.A(rdata_A), .B(ALU_B), .ALU_operation(),  
            .overflow(), 
            .res(ALU_res), 
            .zero());
+	
+	wire [31:0]w_data;
+	MUX2T1_32 mux2_32_3(.I0(ALU_res), .I1(Data_in), .s(MemtoReg), .o(w_data));
 				 
 	
 endmodule
