69561c5c363478d8fb73483ae3227e7c6535839b
ZhouxiangZhan <3150102092@zju.edu.cn>
2017-05-01 18:38:40 +0800

    update Jr Jalr Jal and modify slt andi..

--- Single_Cycle_CPU/SCPU.v
+++ Single_Cycle_CPU/SCPU.v
@@ -48,18 +48,22 @@ module SCPU(clk,
 
 	wire [2:0]ALUop;
 	wire [31:0]addr;
+	wire [4:0]temp_reg;
 	wire [4:0]w_reg;
 	wire [31:0]rdata_A, rdata_B;
 	wire [31:0]ALU_B;
 	wire [31:0]sign_shamt;
 	wire [31:0]ALU_res;
 
+	wire [31:0]temp_data_lui;
+	wire [31:0]temp_data_jal;
 	wire [31:0]w_data;
-	wire [31:0]w_data_n;
 	wire [31:0]offset;
 	wire [31:0]jp_src;
+	wire [31:0]jp_res;
 	wire [31:0]off_s2;
 	wire [31:0]lui_off;
+	wire [31:0]jr_addr;
 	
 	single_pc PC(.clk(clk), .rst(reset), .i_pc(i_pc), .o_pc(o_pc));
 	assign PC_out = o_pc;
@@ -67,24 +71,28 @@ module SCPU(clk,
 	Ext_32 se(.imm_16(inst_in[15:0]), .signal(signal), .Imm_32(sign_shamt));
 
 	control CTRL(.opcode(inst_in[31:26]), .funct(inst_in[5:0]), .RegDst(RegDst), .Branch(Branch), .MemRead(MemRead), .MemtoReg(MemtoReg), 
-				 .ALUop(ALUop), .MemWrite(MemWrite), .ALUSrc(ALUSrc), .RegWrite(RegWrite), .Jump(Jump), .BNE(BNE), .LUI(LUI), signal(signal));
+				 .ALUop(ALUop), .MemWrite(MemWrite), .ALUSrc(ALUSrc), .RegWrite(RegWrite), .Jump(Jump), .BNE(BNE), .LUI(LUI), signal(signal),
+				 .Jal(Jal), .Jr(Jr));
 	assign mem_w = MemWrite;
 				 
-	MUX2T1_5 mux_2_5_inst(.I0(inst_in[20:16]), .I1(inst_in[15:11]), .s(RegDst), .o(w_reg));		 
+	MUX2T1_5 mux_2_5_inst(.I0(inst_in[20:16]), .I1(inst_in[15:11]), .s(RegDst), .o(temp_reg));	
+	MUX2T1_32 MUX2T1_5_jal(.I0(temp), .I1(5'b11111), .s(Jal), .o(w_reg));	 
 
-	assign lui_off = inst_in << 16;	
-	MUX2T1_32 mux2_32_lui(.I0(w_data), .I1(lui_off), .s(LUI), .o(w_data_n));
+	assign lui_off = inst_in << 16;
+
+	MUX2T1_32 MUX2T1_32_jal(.I0(temp_data_jal), .I1(pc_4), .s(Jal), .o(temp_data_lui));
+	MUX2T1_32 mux2_32_lui(.I0(temp_data_lui), .I1(lui_off), .s(LUI), .o(w_data));
 	regs Registers(.clk(clk), .rst(reset), .reg_Rd_addr_A(inst_in[25:21]), .reg_Rt_addr_B(inst_in[20:16]), .reg_Wt_addr(w_reg), 
-					   .wdata(w_data_n), .we(RegWrite), .rdata_A(rdata_A), .rdata_B(rdata_B));
+					   .wdata(w_data), .we(RegWrite), .rdata_A(rdata_A), .rdata_B(rdata_B));
 	assign Data_out = rdata_B;
-
+	assign jr_addr = rdata_A;
 	MUX2T1_32 mux2_32_alusrc(.I0(rdata_B), .I1(sign_shamt), .s(ALUSrc), .o(ALU_B));
 	
 	ALU alu(.A(rdata_A), .B(ALU_B), .ALUop(ALUop),  
            .result(ALU_res), 
            .zero(zero));
 	assign Addr_out = ALU_res;
-	MUX2T1_32 mux2_32_memreg(.I0(ALU_res), .I1(Data_in), .s(MemtoReg), .o(w_data));
+	MUX2T1_32 mux2_32_memreg(.I0(ALU_res), .I1(Data_in), .s(MemtoReg), .o(temp_data_jal));
 	
 	add_32 PC_4(.a(o_pc), .b(4), .c(pc_4));
 	jump_addr jp(.inst(inst_in[25:0]), .pc_4(pc_4), .addr(addr));
@@ -96,7 +104,7 @@ module SCPU(clk,
 	assign Branch_ctrl = Branch & bne_zero;
 	
 	MUX2T1_32 mux2_32_branch(.I0(pc_4), .I1(offset), .s(Branch_ctrl), .o(jp_src));
-	MUX2T1_32 mux2_32_jp(.I0(jp_src), .I1(addr), .s(Jump), .o(i_pc));
-
+	MUX2T1_32 mux2_32_jp(.I0(jp_src), .I1(addr), .s(Jump), .o(jp_res));
+	MUX2T1_32 mux2_32_jr(.I0(jp_res), .I1(jr_addr), .s(Jr), .o(i_pc));
 	
 endmodule
--- Single_Cycle_CPU/control.v
+++ Single_Cycle_CPU/control.v
@@ -32,22 +32,26 @@ module control(
 			output Jump,
 			output BNE,
 			output LUI,
-			output signal			// distinguish the expand of imm
+			output signal,			// distinguish the expand of imm
+			output Jal,
+			output Jr
     );
 
 	assign RegDst = (opcode == 6'b000000) ? 1 : 0;
 	assign RegWrite = (opcode == 6'b000000 || opcode == 6'h23 || opcode == 6'h8 || opcode == 6'hc || opcode == 6'hd || opcode == 6'ha
-						|| opcode == 6'hf)
+						|| opcode == 6'hf || opcode == 6'h3)
 						? 1 : 0;
 	assign MemtoReg = (opcode == 6'h23) ? 1 : 0;
 	assign MemWrite = (opcode == 6'h2b) ? 1 : 0;
 	assign ALUSrc  = (opcode == 6'h2b || opcode == 6'h23 || opcode == 6'h8 || opcode == 6'hc || opcode == 6'hd || opcode == 6'ha) 
 						? 1 : 0;
-	assign Jump = (opcode == 6'h2) ? 1 : 0;
+	assign Jump = (opcode == 6'h2 || opcode == 6'h3) ? 1 : 0;		// Jal and Jump
 	assign Branch = (opcode == 6'h5 || opcode == 6'h4) ? 1 : 0;
 	assign BNE = (opcode == 6'h5) ? 1 : 0;
 	assign LUI = (opcode == 6'hf) ? 1 : 0;
 	assign signal = (opcode == 6'hc || opcode == 6'hd || opcode == 6'he) ? 1 : 0;
+	assign Jal = (opcode == 6'h3) ? 1 : 0;
+	assign Jr = (opcode == 6'h8 || opcode == 6'h9) ? 1 : 0;			// Jr and Jalr
 	assign ALUop[2] = ( ((funct == 6'b100010 || funct == 6'b101010 || funct == 6'b000010) && opcode == 6'h0) || opcode == 6'h4 || opcode == 6'h5 || opcode == 6'ha) 
 						? 1 : 0;
 	assign ALUop[1] = ( ((funct == 6'b100000 || funct == 6'b100010 || funct == 6'b101010 || funct == 6'h26) && opcode == 6'h0) || opcode == 6'h23 
